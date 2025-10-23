8a10135d59dcd2ff935e7cb5e5a58c6144706428
Nikolaidk3 <hp82fk@student.aau.dk>
2025-10-23 10:54:31 +0200

    playpile og drawpile

--- src/Player.java
+++ src/Player.java
@@ -4,15 +4,15 @@ public class Player {
     public long id;
     public final Map<String, Set<Card>> cards = new LinkedHashMap<>();
 
-    public void addCard(Card card) {
-
-    }
-
     public void canPlayCard(Card card) {
         
     }
 
-    public void playCard(Card card) {
+    public void takeCard(Card card, DrawPile drawPile) {
+
+    }
+
+    public void playCard(Card card, PlayPile playPile) {
 
     }
 
