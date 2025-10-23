7eab889f0fea701d442da3e5a5b58abf529b8d60
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-10-23 11:36:31 +0200

    deck med normale kort

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Card> cards = DrawPile.makeDeck();
+        DrawPile.printDeck(cards);
     }
 }
