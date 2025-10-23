7d622a336c1e64b8bb5ca135e32b60576c42e1d4
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-10-23 11:42:14 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Card> cards = DrawPile.makeDeck();
-        DrawPile.printDeck(cards);
+        List<Card> deck = DrawPile.makeDeck();
+        DrawPile.printDeck(deck);
+
+        Collections.shuffle(deck);
+        DrawPile.printDeck(deck);
     }
 }
