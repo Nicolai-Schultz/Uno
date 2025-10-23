d24991cc8dc5002834b1c52404a8fda7d15813e6
Nikolaidk3 <hp82fk@student.aau.dk>
2025-10-23 10:51:04 +0200

    klasse oprettet

---
+++ src/Player.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Player {
+    public long id;
+    public final Map<String, Set<Card>> cards = new LinkedHashMap<>();
+
+    public void addCard(Card card) {
+
+    }
+
+    public void canPlayCard(Card card) {
+        
+    }
+
+    public void playCard(Card card) {
+
+    }
+
+}
